52d1ed5e8860dc0aa4e94b30959e4329c0bfedc9
Nanosor57GIT-HUB <84918375+Nanosor57GIT-HUB@users.noreply.github.com>
2022-04-18 22:04:05 +0200

    commit1 (prob git)

---
+++ scripts/factories/media.js
@@ -0,0 +1,75 @@
+class VideoMediaSubFactory {
+  static render(video) {
+    return `
+    <video controls  preload="metadata" class="media-card-img lb-target">
+                <source  src="assets/videos/${video}" type="video/mp4">
+              </video>       
+        </div> `;
+  }
+}
+
+class ImageMediaSubFactory {
+  static render(image, title, type, name) {
+    return `   
+     <img class="media-${type}-img lb-target" src="assets/Photos/${name}/${image}" alt="${title}">      
+        </div> `;
+  }
+}
+
+function mediaFactory(data, photographer) {
+  const { id, date, image, likes, title, video } = data;
+  const { name } = photographer;
+
+  //mediaCard
+  function getMediaCardDOM() {
+    const article = document.createElement("article");
+    article.innerHTML = `
+      <div class="media-card" data-id="${id}" data-title="${title}" data-date="${date}" tabindex="0">
+   
+      
+      ${
+        image
+          ? ImageMediaSubFactory.render(image, title, "card", name, likes)
+          : VideoMediaSubFactory.render(video, "card", name, title, likes)
+      }
+      <div class="media-card-text">
+          <span class="media-card-title">${title}</span>
+          <div class="likesByMedia">
+          <i class="fas fa-heart  infos-Likes-Icon2"></i>
+          <i class="far fa-heart  infos-Likes-Icon1"></i>    
+           <p id="media-card-likes">${likes}</p>  
+          </div>
+        </div>
+
+      </div>
+`;
+
+    return article;
+  }
+
+  //lightbox
+  function getMediaSlidesDOM() {
+    const article = document.createElement("article");
+    article.innerHTML = `
+      <div class="slide hide-slide" data-id="${id}" data-title="${title}" data-date="${date}">
+      <div class="slide-container">
+        <div class="slide-media-container">
+        <span class="media-slide-title">${title}</span>
+       
+       
+      ${
+        image
+          ? ImageMediaSubFactory.render(image, title, "slide", name)
+          : VideoMediaSubFactory.render(video, "400", "300", name, title)
+      } 
+
+        
+</div>
+         </div>
+      </div>
+`;
+    return article;
+  }
+
+  return { getMediaCardDOM, getMediaSlidesDOM };
+}
